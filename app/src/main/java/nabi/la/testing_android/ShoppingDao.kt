f98455ddb9b42ccec09406dd90e18165911a4c73
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2022-05-13 18:29:19 +0200

    :construction: Room Database init

---
+++ app/src/main/java/nabi/la/testing_android/ShoppingDao.kt
@@ -0,0 +1,22 @@
+package nabi.la.testing_android
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.selects.select
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(item: ShoppingItem)
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItems(): LiveData<List<ShoppingItem>>
+}
